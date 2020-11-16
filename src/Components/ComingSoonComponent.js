1946a99dd72a5df82d48a6fd0d1ef6612a1417bc
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2020-11-15 22:14:51 -0800

    comming soon first initial

---
+++ src/Components/ComingSoonComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+class ComingSoonComponent extends Component{
+  render(){
+    return (
+      <div>Ola mundo</div>
+    );
+  }
+}
+
+export default ComingSoonComponent;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ComingSoonComponent from './Components/ComingSoonComponent';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ComingSoonComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
