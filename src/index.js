fb7ba820701242cddf5f73a148ed545452d61348
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2020-11-20 17:28:19 -0800

    Coming soon header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { jsx, css, Global } from '@emotion/react/macro';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
+
   <React.StrictMode>
     <ComingSoonComponent />
   </React.StrictMode>,
