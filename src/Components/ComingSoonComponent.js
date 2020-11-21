0547322391f0c1fddf9797abea6c841d149fb114
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2020-11-20 17:28:19 -0800

    Coming soon header

--- src/Components/ComingSoonComponent.js
+++ src/Components/ComingSoonComponent.js
@@ -1,4 +1,4 @@
-import { jsx, css } from '@emotion/react/macro';
+import { jsx, css, Global } from '@emotion/react/macro';
 import styled from '@emotion/styled/macro'
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
@@ -6,85 +6,108 @@ import ReactDOM from "react-dom";
 
 
 
-const Main = styled('div')({
-        background: 'rgb(255,0,0)',
-        background: '-moz-linear-gradient(170deg, rgba(255,0,0,1) 0%, rgba(252,3,3,1) 60%, rgba(252,3,3,1) 100%)',
-        background: '-webkit-linear-gradient(170deg, rgba(255,0,0,1) 0%, rgba(252,3,3,1) 60%, rgba(252,3,3,1) 100%)',
-        background: 'linear-gradient(170deg, rgba(255,0,0,1) 0%, rgba(252,3,3,1) 60%, rgba(252,3,3,1) 100%)',
+const Container = styled('div')({
         filter: 'progid:DXImageTransform.Microsoft.gradient(startColorstr="#ff0000",endColorstr="#fc0303",GradientType=1)',
-        maxWidth: "1920px",
         display: "grid",
-        gridTemplateColumns:'repeat(5, 20% [col-start])',
-        gridTemplateRows: 'repeat(3,auto)',
-        maxHeight: "1080px",
-        position:"relative"
+        gridTemplateColumns:'repeat(5, 20%)',
+        gridTemplateRows: 'repeat(3, auto)',
+        gridGap: '1vh'
         
 })
 
-const header = css`
+const headerColumn = css`
     grid-column: 2 / span 3;
     grid-row: 1;
 `
 
 const title = css`
     opacity: 1;
-    position: absolute;
+    position: relative;
     background-color: rgba(255, 255, 255, 0);
     color: rgba(112, 112, 112, 1);
-    font-size: 4vw;
-    font-weight: 700;
-    font-style: normal;
-    font-family: Script MT;
     text-align: center;
-    margin-top: 0;
-    margin-right: 0;
-    margin-bottom: 0;
-    margin-left: 0;
-    padding-top: 0;
-    padding-right: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-`
-const subHeader = css`
-    grid-column: 2 / span 3;
-    grid-row: 2;
+    font: 5vw 'Arizonia', Helvetica, sans-serif;
+    font-weight: 700;
+    
 `
 
 const subTitle = css`
     opacity: 1;
-    position: absolute;
-    background-color: rgba(255, 255, 255, 0);
+    display: block;
     color: rgba(255, 255, 255, 1);
-    font-size: 4vw;
+    font-size: 2vw;
     font-weight: 700;
-    font-style: normal;
-    font-family: Arial Rounded MT;
+    font-style: italic;
+    font-family: Great Vibes;
     text-align: center;
-    line-height: 78.10000000000001;
+    
+`
+const hrSpan = css`
+    display: block;
+    opacity: 0.550000011920929;
     margin-top: 0;
     margin-right: 0;
     margin-bottom: 0;
     margin-left: 0;
-    padding-top: 0;
-    padding-right: 0;
-    padding-bottom: 0;
-    padding-left: 0;
+    font-size: 1vw;
+     
+`
+const hr = css`
+    background: rgb(255,0,0);
+    background: -moz-linear-gradient(170deg, rgba(255,255,255,1) 21%, rgba(53,200,136,1) 46%);
+    background: -webkit-linear-gradient(170deg, rgba(255,255,255,1) 21%, rgba(53,200,136,1) 46%);
+    background: linear-gradient(170deg, rgba(255,255,255,1) 21%, rgba(53,200,136,1) 46%);
+    border-radius: 65vw;
+    height: 0.05vh;
+`
+
+
+const contentColumn = css`
+    grid-column: 2 / span 3;
+    grid-row: 2;
 `
 
 class ComingSoonComponent extends Component{
   render(){
     return (
-        <Main>
-          <header css={ header }>
-            <h1 css={ title }>Fernando de Matos Araujo</h1>
-            <section css={ subHeader }>
-              <h3 css={ subTitle }>Canada based Web Developer</h3>
+        <Container>
+          <Global 
+            styles={css`
+              html{
+                height:100vh;
+                background: rgb(255,0,0);
+                background: -moz-linear-gradient(170deg, rgba(255,0,0,1) 0%, rgba(252,3,3,1) 60%, rgba(252,3,3,1) 100%);
+                background: -webkit-linear-gradient(170deg, rgba(255,0,0,1) 0%, rgba(252,3,3,1) 60%, rgba(252,3,3,1) 100%);
+                background: linear-gradient(170deg, rgba(255,0,0,1) 0%, rgba(252,3,3,1) 60%, rgba(252,3,3,1) 100%);
+              };
+              body{
+                height: 100vh;
+              };
+              #root{
+                height: 100vh;
+              }
+            `}
+          />
+
+          <div css={ headerColumn }>
+              <header>
+                <h1 css={ title }>Fernando de Matos Araujo
+                  <span css={ subTitle }>Canada based Web Developer</span>
+                  <span css={ hrSpan }><hr css={ hr }></hr></span>
+                </h1>
+              </header>
+          </div>
+          <div css={ contentColumn }>
+            <section>
+              <article>
+                <p>ssssssssssssssssssss</p>
+              </article>
             </section>
-          </header>
-          <main>
-            <p>dddd</p>
-          </main>
-        </Main>
+          </div>
+          
+          
+          
+        </Container>
     );
   }
 }
