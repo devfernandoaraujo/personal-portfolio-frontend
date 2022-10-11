0abcead3492f3cfa64e4478fef78bfbc088462c6
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2022-10-11 00:53:17 -0700

    Adding contact and foot sections

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,11 +1,109 @@
 import React from 'react';
 
 const ContactComponent = () => {
-    return( 
-        <div>
-            <span>Contact</span>
+  return (
+    <section className={'contact-area contact-area-two bg_color'} id="contacts">
+      <div className={'container'}>
+        <div className={'row'}>
+          <div className={'col-lg-5 col-md-12'}>
+            <div className={'get_info'}>
+              <div className={'section_title_two undefined'}>
+                <h6>Get in Touch</h6>
+                <h2>Talk or Meet with Me</h2>
+              </div>
+              <div className={'media get_item'}>
+                <i className={'flaticon-phone'} />
+                <div className={'media-body'}>
+                  <h6>Call Me Now</h6>
+                  <a href="https://fernandomnatosaraujo.com/#">
+                    1-778-814-1987
+                  </a>
+                </div>
+              </div>
+              <div className={'media get_item'}>
+                <i className={'flaticon-chat'} />
+                <div className={'media-body'}>
+                  <h6>Contact Me</h6>
+                  <a href="https://fernandomnatosaraujo.com/#">
+                    contact@fernandomatosaraujo.com
+                  </a>
+                </div>
+              </div>
+              <div className={'media get_item'}>
+                <i className={'flaticon-pin'} />
+                <div className={'media-body'}>
+                  <h6>Get Me Here</h6>
+                  <p>Burnaby - British Columbia, Canada</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={'col-lg-7 col-md-12'}>
+            <div className={'react-reveal input_form'}>
+              <div className={'section_title_two undefined'}>
+                <h6>Contact me</h6>
+                <h2>Let me know here</h2>
+              </div>
+              <form className={'form_class'}>
+                <div className={'row'}>
+                  <div className={'col-lg-6'}>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className={'form-control'}
+                      placeholder="Your Name*"
+                    />
+                    <p />
+                  </div>
+                  <div className={'col-lg-6'}>
+                    <input
+                      type="email"
+                      className={'form-control'}
+                      id="email"
+                      name="email"
+                      placeholder="Your Email*"
+                    />
+                    <p />
+                  </div>
+                  <div className={'col-lg-6'}>
+                    <input
+                      type="text"
+                      id="subject"
+                      name="subject"
+                      className={'form-control'}
+                      placeholder="Subject*"
+                    />
+                    <p />
+                  </div>
+                  <div className={'col-lg-6'}>
+                    <input
+                      type="text"
+                      className={'form-control'}
+                      id="phone"
+                      name="phone"
+                      placeholder="Phone*"
+                    />
+                    <p />
+                  </div>
+                </div>
+                <textarea
+                  name="message"
+                  id="message"
+                  className={'form-control'}
+                  rows="6"
+                  placeholder="Your Message ..."
+                />
+                <button type="submit" className={'btn send_btn theme_btn'}>
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </section>
+  );
+};
 
 export default ContactComponent;
