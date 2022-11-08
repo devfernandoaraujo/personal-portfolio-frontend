b0e98707016d7a4d5acf8f46439c5b20be51c6af
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2022-11-08 10:45:59 -0800

    Fixing the alert message effect after submitting

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,20 @@ import TestimonialComponent from './Components/Testimonial';
 import ContactComponent from './Components/Contact';
 import FooterComponent from './Components/Footer';
 
-import '../node_modules/slick-carousel/slick/slick.css';
-import '../node_modules/slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-const App = () => {
-  return (
-    <div className="body_wrapper">
-      <HomeComponent />
-      <AboutComponent />
-      <ServicesComponent />
-      <PortfolioComponent />
-      <SkillsComponent />
-      <TestimonialComponent />
-      <ContactComponent />
-      <FooterComponent />
-    </div>
-  );
-};
+const App = () => (
+	<div className="body_wrapper">
+		<HomeComponent />
+		<AboutComponent />
+		<ServicesComponent />
+		<PortfolioComponent />
+		<SkillsComponent />
+		<TestimonialComponent />
+		<ContactComponent />
+		<FooterComponent />
+	</div>
+);
 
 export default App;
--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const PortfolioComponent = () =>{
-    return(
-        <div>
-            <span>Portfolio</span>
-        </div>
-    );
-}
+const PortfolioComponent = () => {
+  return (
+    <div className={'portfolio_area_two bg_color'} id="portfolio">
+      <span>Portfolio</span>
+    </div>
+  );
+};
 
 export default PortfolioComponent;
