58d477c04604ae599e3f8db412cd62f5d6d93e74
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2024-03-08 17:22:55 -0800

    Adding build folder

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-   server: {
-    port: process.env.PORT || 3000,
-  },
 };
 
 module.exports = nextConfig;
