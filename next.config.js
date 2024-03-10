bb0d572c79f7e683ecb435167f293fbea2d772f4
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2024-03-09 17:20:44 -0800

    Adding build folder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
-const nextConfig = {
-  distDir: 'build',
+const nextConfig = { 
   reactStrictMode: true,
   skipMiddlewareUrlNormalize: true,
   sassOptions: {
