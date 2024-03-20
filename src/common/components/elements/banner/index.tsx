a217cf8efe8fb225c5d6de9fc0065795ac5c82a0
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2024-03-19 20:55:25 -0700

    Implementing schema.org markdown

--- src/common/components/elements/banner/index.tsx
+++ src/common/components/elements/banner/index.tsx
@@ -21,8 +21,8 @@ const BannerComponent = () => {
   }, []);
 
   return (
-    <section className={degrade ? 'frelencer_banner_area frelencer_banner_area-degrade' : 'frelencer_banner_area'} id="home">
-      <div className={'home_bubble'}>
+    <section itemScope itemType="http://schema.org/WPHeader" className={degrade ? 'frelencer_banner_area frelencer_banner_area-degrade' : 'frelencer_banner_area'} id="home">
+      <div itemScope itemType="http://schema.org/CreativeWork" className={'home_bubble'}>
         <div className={'bubble b_one'} />
         <div className={'bubble b_two'} />
         <div className={'bubble b_three'} />
@@ -30,7 +30,7 @@ const BannerComponent = () => {
         <div className={'bubble b_five'} />
         <div className={'bubble b_six'} />
       </div>
-      <Image className={styles.banner + ' react-reveal fadeInRight s_shap'} src="/images/banner_img.d852f421.svg" width="0" height="0" alt="banner" />
+      <Image itemProp="image" className={styles.banner + ' react-reveal fadeInRight s_shap'} src="/images/banner_img.d852f421.svg" width="0" height="0" alt="banner" />
       <div className={'container'}>
         <div className={'row'}>
           <div className={'col-lg-6'}>
@@ -49,42 +49,42 @@ const BannerComponent = () => {
           </div>
           <div className={'col-lg-6'}>
             <div className={'frelencer_img'}>
-              <Image
+              <Image itemProp="image"
                 className={styles.imageOne + ' react-reveal fadeInLeft f_img_one'}
                 src="/images/f-man20b27179.webp"
                 alt="f-man20b27179"
                 width="179"
                 height="236"
               />
-              <Image
+              <Image itemProp="image"
                 className={styles.imageTwo + ' react-reveal fadeInRight f_img_two'}
                 src="/images/table1040818b.webp"
                 alt="table1040818b"
                 width="339"
                 height="246"
               />
-              <Image
+              <Image itemProp="image"
                 className={'react-reveal fadeInDown f_img_three sticky-top ' + styles.imageThree}
                 src={`data:image/webp;base64,${binaryImages.bannerImages.imageThree}`}
                 alt="imageThree"
                 width={162}
                 height={69}
               />
-              <Image className="react-reveal fadeInDown f_img_four imageFour" src="/images/shadow1add9ee7.webp" alt="shadow1add9ee7" width={412} height={267} />
+              <Image itemProp="image" className="react-reveal fadeInDown f_img_four imageFour" src="/images/shadow1add9ee7.webp" alt="shadow1add9ee7" width={412} height={267} />
               <div className={'react-reveal fadeInDown f_img_five ' + styles.imageFive}>
-                <Image className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageFive}`} alt="imageFive" width={56} height={83} />
+                <Image  itemProp="image"className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageFive}`} alt="imageFive" width={56} height={83} />
               </div>
               <div className={'f_img_six'}>
-                <Image className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageSix}`} alt="imageSix" width={56} height={83} />
+                <Image itemProp="image" className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageSix}`} alt="imageSix" width={56} height={83} />
               </div>
               <div className={'f_img_seven'}>
-                <Image className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageSeven}`} alt="img_seven" width={56} height={83} />
+                <Image itemProp="image" className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageSeven}`} alt="img_seven" width={56} height={83} />
               </div>
               <div className={'f_img_eight'}>
-                <Image className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageEight}`} alt="img_eight" width={56} height={83} />
+                <Image itemProp="image" className="" src={`data:image/webp;base64,${binaryImages.bannerImages.imageEight}`} alt="img_eight" width={56} height={83} />
               </div>
               <div className={'f_img_nine'}>
-                <Image className="" src={`data:image/webp;base64, ${binaryImages.bannerImages.imageNine}`} alt="img_nine" width={56} height={83} />
+                <Image itemProp="image" className="" src={`data:image/webp;base64, ${binaryImages.bannerImages.imageNine}`} alt="img_nine" width={56} height={83} />
               </div>
             </div>
           </div>
