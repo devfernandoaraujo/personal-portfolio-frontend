a7adc02767e215b36fe3fad6640efc50b2d2f29b
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2024-03-19 20:12:02 -0700

    Implementing Zustand

--- src/common/utils/alert.tsx
+++ src/common/utils/alert.tsx
@@ -1,7 +1,7 @@
 
 'use client'
 import React, { useEffect, useState } from 'react';
-import styles from '@/styles/alert.module.scss';
+import styles from '@/styles/Alert.module.scss';
 import { useAlertVisible, useAlertVisibleActions } from '@/common/stores';
 
 export interface IAlertPops {
